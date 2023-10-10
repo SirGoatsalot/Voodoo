10b7b31f3e1dc9b5558f2b0ce7ff4438a493fd30
Foster Sullivan <fostersullivan12@gmail.com>
2023-10-09 23:35:05 -0400

    Implement error handling backbone.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::{env, process, error::Error, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+struct Config {
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            Err("Not Enough arguments!")
+        } else {
+            Ok(
+                Config {
+                    file_path: args[1].clone()
+                }
+            )
+        }
+    }
+}
+ 
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_path)?;
+    
+    println!("Read Contents: \n{contents}");
+
+    Ok(())
 }
